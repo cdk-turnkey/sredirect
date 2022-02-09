2d43f65a8137c79d941902bbab7067b9238a4d6a
Douglas Naphas <douglasnaphas@gmail.com>
2022-02-08 17:39:42 -0700

    Actually assert something about the stack
    gh-4

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,3 +1,4 @@
+import { Capture, Match, Template } from "aws-cdk-lib/assertions";
 import * as cdk from "aws-cdk-lib";
 import * as Lib from "../lib";
 import { RedirectType } from "../lib/RedirectType";
@@ -19,4 +20,16 @@ test("can instantiate app stack", () => {
       new Lib.Redirect("abc.com", "https://example.com", RedirectType.FOUND),
     ],
   });
+});
+test("app stack contains a Hosted Zone", () => {
+  const app = new cdk.App();
+  process.env.GITHUB_REPOSITORY = "githubuser/repo-name";
+  process.env.GITHUB_REF = "refs/heads/master";
+  const stack = new Lib.AppStack(app, "MyTestApp", {
+    redirects: [
+      new Lib.Redirect("abc.com", "https://example.com", RedirectType.FOUND),
+    ],
+  });
+  const template = Template.fromStack(stack);
+  template.resourceCountIs("AWS::Route53::HostedZone", 1);
 });
