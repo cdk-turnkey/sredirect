4aa740195b0bd0f222b6bb6c726bff2bb891f3e0
Douglas Naphas <douglasnaphas@gmail.com>
2022-02-08 22:23:57 -0700

    Capture the CFF
    gh-4

--- test/app.test.ts
+++ test/app.test.ts
@@ -5,17 +5,18 @@ import { RedirectType } from "../lib/RedirectType";
 import { URL } from "url";
 
 const OLD_ENV = process.env;
+let app: cdk.App;
 beforeEach(() => {
   jest.resetModules();
   process.env = { ...OLD_ENV };
+  process.env.GITHUB_REPOSITORY = "githubuser/repo-name";
+  process.env.GITHUB_REF = "refs/heads/master";
+  app = new cdk.App();
 });
 afterAll(() => {
   process.env = { ...OLD_ENV };
 });
 test("can instantiate app stack", () => {
-  const app = new cdk.App();
-  process.env.GITHUB_REPOSITORY = "githubuser/repo-name";
-  process.env.GITHUB_REF = "refs/heads/master";
   const stack = new Lib.AppStack(app, "MyTestApp", {
     redirects: [
       new Lib.Redirect(
@@ -27,9 +28,6 @@ test("can instantiate app stack", () => {
   });
 });
 test("app stack contains a Hosted Zone", () => {
-  const app = new cdk.App();
-  process.env.GITHUB_REPOSITORY = "githubuser/repo-name";
-  process.env.GITHUB_REF = "refs/heads/master";
   const stack = new Lib.AppStack(app, "MyTestApp", {
     redirects: [
       new Lib.Redirect(
@@ -41,4 +39,31 @@ test("app stack contains a Hosted Zone", () => {
   });
   const template = Template.fromStack(stack);
   template.resourceCountIs("AWS::Route53::HostedZone", 1);
+});
+test("stack has the expected CFF 1", () => {
+  const stack = new Lib.AppStack(app, "MyTestApp", {
+    redirects: [
+      new Lib.Redirect(
+        new URL("https://abc.com"),
+        new URL("https://example.com"),
+        RedirectType.FOUND
+      ),
+    ],
+  });
+  const template = Template.fromStack(stack);
+  const distributionConfigCapture = new Capture();
+  template.hasResourceProperties("AWS::CloudFront::Distribution", {
+    DistributionConfig: distributionConfigCapture,
+  });
+  const cffCapture = new Capture();
+  // console.log(
+  //   distributionConfigCapture.asObject().DefaultCacheBehavior
+  //     .FunctionAssociations[0]
+  // );
+  template.hasResourceProperties("AWS::CloudFront::Distribution", {
+    DistributionConfig: Match.objectLike({
+      DefaultCacheBehavior: { FunctionAssociations: [cffCapture] },
+    }),
+  });
+  console.log(cffCapture);
 });
