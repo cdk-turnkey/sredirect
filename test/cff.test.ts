e148ec26a467246159a1ca76e14362630d715111
Douglas Naphas <douglasnaphas@gmail.com>
2022-02-10 19:56:28 -0700

    Fix path to host value in CFF event
    gh-4
    
    I was acting like event.request.host would have the host, but really it
    will be at event.request.headers.host.
    
    https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/functions-event-structure.html

--- test/cff.test.ts
+++ test/cff.test.ts
@@ -1,6 +1,6 @@
 describe("CFF code for 'stack has the expected CFF(s) 3'", () => {
   describe("path /", () => {
-    const handler = (event: any): any => {
+    const handlerWithNestedFor = (event: any): any => {
       var legend: any = [
         {
           host: "fromme.com",
@@ -20,54 +20,66 @@ describe("CFF code for 'stack has the expected CFF(s) 3'", () => {
       };
 
       outer: for (var i = 0; i < legend.length; i++) {
-        if (legend[i].host != request.host) {
-          console.log("legend host:");
-          console.log(legend[i].host);
-          console.log("request host:");
-          console.log(request.host);
-          console.log("mismatch 0");
+        if (legend[i].host != request.headers.host) {
           continue outer;
         }
         var querystringEntries: any = Object.entries(legend[i].querystring);
         inner: for (var j = 0; j < querystringEntries.length; j++) {
           if (!request.querystring[querystringEntries[j][0]]) {
-            console.log("request:");
-            console.log(request);
-            console.log("querystringEntries[j].key:");
-            console.log(querystringEntries[j][0]);
-            console.log("mismatch 1");
             continue outer;
           }
           if (
             request.querystring[querystringEntries[j][0]] !=
             querystringEntries[j][1]
           ) {
-            console.log("mismatch 2");
             continue outer;
           }
         }
-        console.log("match!");
         response.headers = { location: { value: legend[i].value } };
         return response;
       }
       return { statusCode: 404, statusDescription: "Not Found" };
     };
+    const handlerWithMapLookup = (event: any): any => {
+      var legend: any = {
+        "fromme.com": [
+          {
+            querystring: { "participant-id": "4499" },
+            locationValue: "https://docs.google.com/edit/zzbbaajj",
+          },
+        ],
+        "other-fromme.com": [
+          {
+            querystring: {},
+            locationValue: "https://tome.com",
+          },
+        ],
+      };
+      var request = event.request;
+      var response: any = {
+        statusCode: 302,
+        statusDescription: "Found",
+      };
+      if (!legend[request.headers && request.headers.host]) {
+      }
+    };
     test.each([
       {
         querystring: { "participant-id": "4499" },
-        host: "fromme.com",
+        headers: { host: "fromme.com" },
         expected: "https://docs.google.com/edit/zzbbaajj",
       },
       {
         querystring: {},
-        host: "other-fromme.com",
+        headers: { host: "other-fromme.com" },
         expected: "https://tome.com",
       },
     ])(
       "$querystring, $host -> $expected",
-      ({ querystring, host, expected }) => {
+      ({ querystring, headers, expected }) => {
         expect(
-          handler({ request: { querystring, host } }).headers.location.value
+          handlerWithNestedFor({ request: { querystring, headers } }).headers
+            .location.value
         ).toEqual(expected);
       }
     );
